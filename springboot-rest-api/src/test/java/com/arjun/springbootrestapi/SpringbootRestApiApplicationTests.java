794ef3e1c0187d59c4e9066f6a7727b67cf9bb82
MrUnwonted <arjungodzarjun@gmail.com>
2023-09-11 15:11:54 +0530

    New

---
+++ springboot-rest-api/src/test/java/com/arjun/springbootrestapi/SpringbootRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arjun.springbootrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
